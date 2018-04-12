198af9b2264fbb78eb7339f0fda4ea29e02104b0
GitHub-bigT <564241248@qq.com>
2018-04-12 18:37:12 +0800

    create inner shape

--- BaseGeometryPrimitive/BaseGeometryPrimitive/main.cpp
+++ BaseGeometryPrimitive/BaseGeometryPrimitive/main.cpp
@@ -6,18 +6,23 @@
 #include <osg/Geode>
 #include <osg/Node>
 #include <osg/LineWidth>
+#include <osg/Shape>
+#include <osg/ShapeDrawable>
 
 std::string osgModelPath = "..//..//..//OSGPractise//Resource//";
 
 osg::ref_ptr<osg::Node> createQuad();
 osg::ref_ptr<osg::Node> createLine();
+osg::ref_ptr<osg::Node> createShape();
 
 int main(int argc, char** argv)
 {
 	osgViewer::Viewer viewer;
 	osg::ref_ptr<osg::Group> root = new osg::Group;
-	root->addChild(createQuad());
+	root->addChild(createShape());
+
 	viewer.setSceneData(root);
+
 	viewer.realize();
 	viewer.run();
 }
@@ -27,11 +32,12 @@ osg::ref_ptr<osg::Node> createQuad()
 	osg::ref_ptr<osg::Geometry> geom = new osg::Geometry;
 
 	osg::ref_ptr<osg::Vec3Array> v = new osg::Vec3Array;
+	geom->setVertexArray(v);
+
 	v->push_back(osg::Vec3(-1.0f, 0.0f, -1.0f));
 	v->push_back(osg::Vec3(1.0f, 0.0f, -1.0f));
 	v->push_back(osg::Vec3(1.0f, 0.0f, 1.0f));
 	v->push_back(osg::Vec3(-1.0f, 0.0f, 1.0f));
-	geom->setVertexArray(v);
 
 	osg::ref_ptr<osg::Vec3Array> c = new osg::Vec3Array;
 	c->push_back(osg::Vec3(1.0f, 0.0f, 0.0f));
@@ -86,4 +92,24 @@ osg::ref_ptr<osg::Node> createLine()
 	osg::ref_ptr<osg::Geode> geo = new osg::Geode;
 	geo->addDrawable(geom);
 	return geo;
+}
+
+osg::ref_ptr<osg::Node> createShape()
+{
+	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+	float radius = 0.08f;
+	float height = 0.1f;
+
+	osg::ref_ptr<osg::TessellationHints> hint = new osg::TessellationHints;
+	hint->setDetailRatio(0.9f);
+
+	osg::ref_ptr<osg::Sphere> sphere = new osg::Sphere(osg::Vec3(-1.0f, 0.0f, 0.0f), radius);
+	//geode->addDrawable(new osg::ShapeDrawable(sphere, hint));
+	geode->addDrawable(new osg::ShapeDrawable(new osg::Sphere(osg::Vec3(0.0f, 0.0f, 0.0f), radius), hint));
+	geode->addDrawable(new osg::ShapeDrawable(new osg::Box(osg::Vec3(0.2f, 0.0f, 0.0f), radius), hint));
+	geode->addDrawable(new osg::ShapeDrawable(new osg::Cone(osg::Vec3(0.4f, 0.0f, 0.0f), radius, height), hint));
+	geode->addDrawable(new osg::ShapeDrawable(new osg::Cylinder(osg::Vec3(0.6f, 0.0f, 0.0f), radius, height), hint));
+	geode->addDrawable(new osg::ShapeDrawable(new osg::Capsule(osg::Vec3(0.8f, 0.0f, 0.0f), radius, height), hint));
+
+	return geode.get();
 }
