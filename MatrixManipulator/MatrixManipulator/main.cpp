b0281f3253a8becf4135c903d6867e51e8951bbc
GitHub-bigT <564241248@qq.com>
2018-04-16 09:40:20 +0800

    add empty Matrix Manipulator

---
+++ MatrixManipulator/MatrixManipulator/main.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <windows.h>
+
+#include <osgViewer/Viewer>
+
+std::string osgModelPath("..//..//..//OSGPractise//Resource//");
+
+int main(int argc, char** argv)
+{
+	return -1;
+}
