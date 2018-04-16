eb23c7839f464daf9913e304adff37dd7a7ab9db
GitHub-bigT <564241248@qq.com>
2018-04-16 09:09:22 +0800

    osg decrease window

---
+++ interactive/interactive/PickEventHnadler.h
@@ -0,0 +1,67 @@
+#pragma once
+#include <osgViewer/Viewer>
+#include <osgGA/GUIEventHandler>
+#include <osgUtil/LineSegmentIntersector>
+
+class PickEventHnadler : public osgGA::GUIEventHandler
+{
+public:
+	PickEventHnadler();
+	~PickEventHnadler();
+
+	virtual bool handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa)
+	{ 
+		osgViewer::Viewer *viewer = dynamic_cast<osgViewer::Viewer*>(&aa);
+		switch (ea.getEventType())
+		{
+		case osgGA::GUIEventAdapter::EventType::PUSH:
+		{
+			if (ea.getButton() == 4)
+			{
+				pick(ea.getX(), ea.getY(), *viewer);
+			}
+			break;
+		}
+		default:
+			break;
+		}
+		return false; 
+	}
+
+	void pick(float x, float y, osgViewer::Viewer &viewer)
+	{
+		printf("x = %f, y = %f\n", x, y);
+		osgUtil::LineSegmentIntersector::Intersections intersections;
+		if (viewer.computeIntersections(x, y, intersections))
+		{
+			//printf("intersector\n");
+			osgUtil::LineSegmentIntersector::Intersections::iterator iter;
+			for (iter = intersections.begin(); iter != intersections.end(); iter++)
+			{
+				if (!iter->nodePath.empty() && !(iter->nodePath.back()->getName().empty()))
+				{
+					osg::NodePath np = iter->nodePath;
+					for (int i = 0; i < np.size(); i++)
+					{
+						osg::Node *node = dynamic_cast<osg::Node*>(np[i]);
+						node->setNodeMask(0);
+					}
+				}
+			}
+		}
+		else
+		{
+			printf("no intersector\n");
+		}
+	}
+private:
+
+};
+
+PickEventHnadler::PickEventHnadler()
+{
+}
+
+PickEventHnadler::~PickEventHnadler()
+{
+}
--- interactive/interactive/main.cpp
+++ interactive/interactive/main.cpp
@@ -1,19 +1,42 @@
 #include <string>
 #include <windows.h>
 
+#include <osgFX/Scribe>
+
 #include "UseEventHandle.h"
+#include "PickEventHnadler.h"
 
 std::string osgModelPath("..//..//..//OSGPractise//Resource//");
 
 int main(int argc, char** argv)
 {
 	osgViewer::Viewer viewer;
+	osg::ref_ptr<osg::GraphicsContext::Traits> traits = new osg::GraphicsContext::Traits;
+	traits->x = 40;
+	traits->y = 40;
+	traits->width = 800;
+	traits->height = 600;
+	traits->windowDecoration = true;
+	traits->doubleBuffer = true;
+	traits->sharedContext = 0;
+	osg::ref_ptr<osg::GraphicsContext> gc = osg::GraphicsContext::createGraphicsContext(traits);
+	osg::ref_ptr<osg::Camera> camera = new osg::Camera;
+	camera->setGraphicsContext(gc);
+	camera->setViewport(new osg::Viewport(0, 0, traits->width, traits->height));
+/*
+	GLenum buffer = traits->doubleBuffer ? GL_BACK : GL_FRONT;
+	camera->setDrawBuffer(buffer);
+	camera->setReadBuffer(buffer);*/
+	viewer.addSlave(camera);
 
 	osg::ref_ptr<osg::Group> root = new osg::Group;
-	root->addChild(osgDB::readNodeFile(osgModelPath + "glider.osg"));
-	root->addChild(osgDB::readNodeFile(osgModelPath + "osgcool.osgt"));
+	root->addChild(osgDB::readNodeFile(osgModelPath + "cessna.osgt"));
+	osg::ref_ptr<osg::Node> cow = osgDB::readNodeFile(osgModelPath + "cow.osg");
+	osg::ref_ptr<osgFX::Scribe> sc = new osgFX::Scribe;
+	sc->addChild(cow);
+	root->addChild(sc);
 	viewer.setSceneData(root);
-	viewer.addEventHandler(new UseEventHandle);
+	viewer.addEventHandler(new PickEventHnadler);
 	viewer.realize();
 	viewer.run();
 
