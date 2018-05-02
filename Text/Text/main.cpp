9a579c170dff5d7a2f5e732b4673f5eb84d2158f
GitHub-bigT <564241248@qq.com>
2018-05-02 09:44:42 +0800

    add text

---
+++ Text/Text/main.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <windows.h>
+
+#include <osgViewer/Viewer>
+#include <osgDB/ReadFile>
+
+std::string osgModelPath("..//..//..//OSGPractise//Resource//");
+
+void setupWindow(osgViewer::Viewer &viewer)
+{
+	osg::ref_ptr<osg::GraphicsContext::Traits> traits = new osg::GraphicsContext::Traits;
+	traits->x = 1920 + 60;
+	traits->y = 60;
+	traits->width = 800;
+	traits->height = 600;
+	traits->windowDecoration = true;
+	traits->doubleBuffer = true;
+	traits->sharedContext = 0;
+	osg::ref_ptr<osg::GraphicsContext> gc = osg::GraphicsContext::createGraphicsContext(traits);
+	osg::ref_ptr<osg::Camera> camera = new osg::Camera;
+	camera->setGraphicsContext(gc);
+	camera->setViewport(new osg::Viewport(0, 0, traits->width, traits->height));
+	/*
+	GLenum buffer = traits->doubleBuffer ? GL_BACK : GL_FRONT;
+	camera->setDrawBuffer(buffer);
+	camera->setReadBuffer(buffer);*/
+	viewer.addSlave(camera);
+}
+
+int main(int argc, char** argv)
+{
+	osgViewer::Viewer viewer;
+	osg::ref_ptr<osg::Group> root = new osg::Group();
+	setupWindow(viewer);
+	osg::ref_ptr<osg::Node> cow = osgDB::readNodeFile(osgModelPath + "cow.osgt");
+	root->addChild(cow);
+	viewer.setSceneData(root);
+
+	viewer.realize();
+	viewer.run();
+	return -1;
+}
