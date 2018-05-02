4857a0ae6c0f6c9119a229acd39c1c6558b0bf69
GitHub-bigT <564241248@qq.com>
2018-05-02 19:38:47 +0800

    update

--- Text/Text/main.cpp
+++ Text/Text/main.cpp
@@ -3,8 +3,10 @@
 
 #include <osgViewer/Viewer>
 #include <osgDB/ReadFile>
+#include <osgText/Text>
+#include <osg/Camera>
 
-std::string osgModelPath("..//..//..//OSGPractise//Resource//");
+std::string osgModelPath("../../../OSGPractise/Resource/");
 
 void setupWindow(osgViewer::Viewer &viewer)
 {
@@ -27,12 +29,32 @@ void setupWindow(osgViewer::Viewer &viewer)
 	viewer.addSlave(camera);
 }
 
+osg::Node* createHUD()
+{
+	osgText::Text *text = new osgText::Text;
+	text->setFont(osgModelPath + "fonts/times.ttf");
+	text->setText("aaAAbb");
+	text->setPosition(osg::Vec3(100.0f, 200.0f, 0.0f));
+	osg::Camera *camera = new osg::Camera;
+	camera->setProjectionMatrix(osg::Matrix::ortho2D(0, 800, 0, 600));
+	camera->setViewMatrix(osg::Matrix::identity());
+	camera->setReferenceFrame(osg::Transform::ABSOLUTE_RF);
+	camera->setClearMask(GL_DEPTH_BUFFER_BIT);
+	camera->setRenderOrder(osg::Camera::RenderOrder::POST_RENDER);
+	camera->addChild(text);
+	return camera;
+}
+
 int main(int argc, char** argv)
 {
 	osgViewer::Viewer viewer;
-	osg::ref_ptr<osg::Group> root = new osg::Group();
 	setupWindow(viewer);
-	osg::ref_ptr<osg::Node> cow = osgDB::readNodeFile(osgModelPath + "ceep.ive");
+
+	osg::ref_ptr<osg::Group> root = new osg::Group();
+	osg::ref_ptr<osg::Node> glider = osgDB::readNodeFile(osgModelPath + "glider.osgt");
+	root->addChild(glider);
+	root->addChild(createHUD());
+	osg::ref_ptr<osg::Node> cow = osgDB::readNodeFile(osgModelPath + "cow.osgt");
 	root->addChild(cow);
 	viewer.setSceneData(root);
 	viewer.realize();
